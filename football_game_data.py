653da9dde61b736038be16c5fad041ba0de3c963
Viktor Boiarchuk <victorboiarchuk@gmail.com>
2017-07-07 18:16:23 +0300

    more changes and some fixes

--- football_game_data.py
+++ football_game_data.py
@@ -3,10 +3,25 @@ from openerp.osv import osv, fields
 
 class football_game_data(osv.osv):
     _name = 'football.game.data'
+
+    def defining_team_satisfaction(self, cr, uid, ids, name, args, context={}):
+        if not ids:
+            return {}
+        res = {}.fromkeys(ids, [])
+        team_pool = self.read(cr, uid, ids, ['team'], context=context)
+        for ind, record in enumerate(ids):
+            if team_pool[ind]['team'] == '1':
+                res[record] = 'Satisfied with the team !'
+            else:
+                res[record] = 'Disappointed with the team !'
+        return res
+
     _columns = {
-        'team': fields.selection([('1', 'Team #1'), ('2', 'Team #2')], 'Team', required=True),
+        'team': fields.selection([('1', 'Team #1 (Winners)'), ('2', 'Team #2 (Losers)')], 'Team', required=True),
         'game_id': fields.many2one('football.game', 'Game'),
         'player_id': fields.many2one('football.player', 'Player'),
+        'team_satisfaction': fields.function(defining_team_satisfaction, string='Team satisfaction', method=True,
+                                             type='char'),
     }
     _order = 'team'
 
--- football_player.py
+++ football_player.py
@@ -1,9 +1,13 @@
 from openerp.osv import osv, fields
 from datetime import datetime
+from random import sample
 
 
 class football_player(osv.osv):
     _name = 'football.player'
+
+
+
     _columns = {
         'name': fields.char('Player name', size=32, required=True),
         'surname': fields.char('Player surname', size=32, required=True),
@@ -13,8 +17,7 @@ class football_player(osv.osv):
         'stadium_ids': fields.many2many('football.stadium', 'player_stadium_rel',
                                         'player_id', 'stadium_id', 'Lucky stadiums'),
         'game_id': fields.many2one('football.game', 'Player\'s game participation'),
-        'team_1_ids': fields.one2many('wizard/football.wizard', 'team_1_id', 'Team #1'),
-
+        # 'team_1_ids': fields.one2many('wizard/football.wizard', 'team_1_id', 'Team #1'),
     }
 
     def onchange_age(self, cr, uid, ids, date_of_birth, context={}):
@@ -22,8 +25,8 @@ class football_player(osv.osv):
         return {'value': {'age': real_age}}
 
     def name_get(self, cr, user, ids, context={}):
-        # if not ids:
-        #     return []
+        if not ids:
+            return []
         res = []
         for record in self.read(cr, user, ids, ['name', 'surname'], context=context):
             name = record['name']
@@ -33,11 +36,20 @@ class football_player(osv.osv):
         return res
 
     def resulted_table_creating(self, cr, uid, ids, context={}):
-        default_game = {'name': 'Weekly Enapps\'s game', 'duration': 1.5}
+        stadium = self.pool.get('football.stadium').search(cr, uid, [('name', '=', 'Markevich football center')],
+                                                           context=context)
+        game_player_pool = self.pool.get('football.game.data')
+        happiness = game_player_pool.read(cr, uid, ids, ['team_satisfaction'], context=context)
+        default_game = {'name': 'Weekly Enapps\'s game', 'datetime': datetime.today().date(), 'stadium_id': stadium and stadium[0],
+                        'duration': 1.5}
         game = self.pool.get('football.game').create(cr, uid, default_game, context=context)
-        game_player_data_pool = self.pool.get('football.game.data')
-        for player_id in ids:
-            game_player_data_pool.create(cr, uid, {'game_id': game, 'player_id': player_id, 'team': '1'}, context=context)
+        team_gen = [str(2) for dummy_x in range(len(ids))]
+        for i in sample(range(len(ids)), len(ids)/2):
+            team_gen[i] = '1'
+        for ind, player_id in enumerate(ids):
+            game_player_pool.create(cr, uid, {'game_id': game, 'player_id': player_id, 'team': team_gen[ind],
+                                              'team_satisfaction': happiness[ind]['team_satisfaction']},
+                                    context=context)
         return {'name': 'football game',
                 'view_mode': 'form',
                 'view_type': 'form',
